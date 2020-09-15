54ab7c505a4522d399d9b143b592c09bb5918424
sesethud <dsesethu60@gmail.com>
2020-09-15 16:30:17 +0200

    codeStructure.md added

---
+++ codeStructure.md
@@ -0,0 +1,12 @@
+### Steps
+
+Phumlani is working on the 
+```get("/login```
+
+
+Sino is working on the```get("/patient``` alongside the 
+patient.handlebars
+
+
+Sesethu is working on ```get("/epharmacy```
+
--- src/main/java/co/projectcodex/hackathon/App.java
+++ src/main/java/co/projectcodex/hackathon/App.java
@@ -16,7 +16,7 @@ import static spark.Spark.*;
 
 public class App {
 
-    /*static int getHerokuAssignedPort() {
+    static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
@@ -45,23 +45,10 @@ public class App {
 
         return Jdbi.create(defualtJdbcUrl);
 
-    }*/
-
-
+    }
 
     public static void main(String[] args) {
-
-        // root is 'src/main/resources', so put files in 'src/main/resources/public'
-        staticFiles.location("/public"); // Static files
-
-        get("/", (req, res) -> {
-
-            Map<String, Object> map = new HashMap<>();
-            return new ModelAndView(map, "patient.handlebars");
-
-        }, new HandlebarsTemplateEngine());
-
-        /*try  {
+        try  {
 
 
             staticFiles.location("/public");
@@ -110,7 +97,7 @@ public class App {
 
         } catch (Exception ex) {
             ex.printStackTrace();
-        }*/
+        }
 
     }
 }
